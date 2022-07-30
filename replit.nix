188eb6735c3adc25f3223bddd0928a09cf319174
adityateguh345 <105525145+Adityateguh123@users.noreply.github.com>
2022-07-30 08:29:14 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+	deps = [
+		pkgs.openssh_with_kerberos
+  pkgs.nodejs-16_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.yarn
+        pkgs.replitPackages.jest
+	];
+}
